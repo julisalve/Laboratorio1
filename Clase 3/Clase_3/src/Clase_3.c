879cc0bf05ed0a22850a43582cbf0de9d90ead13
Your Name <you@example.com>
2019-08-28 19:38:39 -0300

    solo falta ver promedio

--- Clase 3/Clase_3/src/Clase_3.c
+++ Clase 3/Clase_3/src/Clase_3.c
@@ -14,30 +14,51 @@
 #define MAX_INT 65535
 
 int calcularNumeroInt(long numeroLong, int *numeroInt);
-
+int calcularMaximo(int numero, int *numeroMax,int *contador);
+int calcularMinimo(int numero, int *numeroMin,int *contador);
+int acumularNumeros(int numero, int *acumuladorNumeros);
 int main(void) {
 	long numeroLong;
 	int numero;
 	int i;
 	int contador=0;
-	int Maximo;
-	for(i=0; i<5; i++)
+	int maximo;
+	int minimo;
+	int acumulador=0;
+	float promedio;
+	for(i=0; i<CANTIDAD_NUMEROS; i++)
 	{
 		printf("Ingrese un numero: \n");
 		scanf("%ld",&numeroLong);
 		if(calcularNumeroInt(numeroLong, &numero)==0)
 		{
 
+            if(calcularMaximo(numero,&maximo,&contador)==0)
+            {
+
+            }
+            if(calcularMinimo(numero, &minimo,&contador)==0)
+            {
+
+            }
+            if(acumularNumeros(numero,&acumulador)==0)
+            {
+
+            }
 		}
+
 		while(calcularNumeroInt(numeroLong, &numero)!=0)
 		{
-			printf("Error, indique otro numero");
+			printf("Error, indique otro numero \n");
 			scanf("%ld", &numeroLong);
 		}
-		if(calcularMaximo(numero, *Maximo,int *contador))
+
 
 	}
-
+    promedio=(float)acumulador/CANTIDAD_NUMEROS;
+    printf("El numero maximo es : %d \n",maximo);
+    printf("El numero minimo es: %d \n",minimo);
+    printf("El promedio es: %2.f \n",promedio);
 
 }
 
@@ -61,7 +82,27 @@ int calcularMaximo(int numero, int *numeroMax,int *contador)
 	{
 		retorno=0;
 		*numeroMax=numero;
-		*contador++;
+		*contador=*contador+1;
 	}
 	return retorno;
+}
+
+int calcularMinimo(int numero, int *numeroMin,int *contador)
+{
+	int retorno=-1;
+	if(*contador==0 || numero<*numeroMin)
+	{
+		retorno=0;
+		*numeroMin=numero;
+		*contador=*contador+1;
+	}
+	return retorno;
+}
+
+int acumularNumeros(int numero, int *acumuladorNumeros)
+{
+    int retorno=0
+    *acumuladorNumeros=*acumuladorNumeros+numero;
+
+
 }
