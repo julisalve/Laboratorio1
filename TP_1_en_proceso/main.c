42c300a8fb9afbdb7caa842446de4b832ef7e8ab
Your Name <you@example.com>
2019-09-01 13:04:31 -0300

    Tp1 finalizado en codeblock, consultar si esta ok

--- TP_1_en_proceso/main.c
+++ TP_1_en_proceso/main.c
@@ -8,9 +8,11 @@ float getFloat(float *resultado,
                char *mensaje,
                char *mensajeError);
 
-float sumarFloat(float primerNumero, float segundoNumero,float *suma);
-
-
+int sumarFloat(float primerNumero, float segundoNumero,float *suma);
+int restarFloat(float primerNumero, float segundoNumero,float *resta);
+int dividirFloat(float primerNumero, float segundoNumero,float *division);
+int multiplicarFloat(float primerNumero, float segundoNumero,float *multiplicacion);
+int factorialDeFloat(float primerNumero, float segundoNumero,float *factorialPrimerNumero, float *factorialSegundoNumero);
 
 int main()
 {
@@ -19,7 +21,13 @@ int main()
     int opcionMenu;
     int flagPrimerOperando=0;
     int flagSegundoOperando=0;
+    int flagCalculosRealizados=0;
     float suma;
+    float resta;
+    float division;
+    float multiplicacion;
+    float factorialPrimerNumero;
+    float factorialSegundoNumero;
 
     do
     {
@@ -39,7 +47,7 @@ int main()
         scanf("%d",&opcionMenu);
 
 
-        while(opcionMenu!=1 && opcionMenu!=2&&(flagPrimerOperando==0 || flagSegundoOperando==0))
+        while(opcionMenu!=1 && opcionMenu!=2 && opcionMenu!=5 && (flagPrimerOperando==0 || flagSegundoOperando==0))
         {
             if(opcionMenu==3 || opcionMenu==4)
             {
@@ -47,7 +55,7 @@ int main()
                 scanf("%d",&opcionMenu);
             }
 
-            else if(opcionMenu>5)
+            else if(opcionMenu>5 || opcionMenu<1)
             {
                 printf("No es una opcion valida del Menu. Reingrese otra opcion. \n");
                 scanf("%d",&opcionMenu);
@@ -73,20 +81,45 @@ int main()
         if(opcionMenu==3)
         {
             sumarFloat(primerOperando, segundoOperando,&suma);
+            restarFloat(primerOperando,segundoOperando,&resta);
+            dividirFloat(primerOperando, segundoOperando,&division);
+            multiplicarFloat(primerOperando, segundoOperando,&multiplicacion);
+            factorialDeFloat(primerOperando, segundoOperando,&factorialPrimerNumero, &factorialSegundoNumero);
+            printf("Calculos realizados con exitos. \n \n");
+            system("pause");
+            flagCalculosRealizados=1;
         }
-        if(opcionMenu==4)
+        if(opcionMenu==4 && flagCalculosRealizados==1)
         {
-            printf("El resultado de A + B es: %.2f \n",&suma);
+            printf("El resultado de A + B es: %.2f \n",suma);
+            printf("El resultado de A - B es: %.2f \n",resta);
+            if(dividirFloat(primerOperando, segundoOperando,&division)==0)
+            {
+                    printf("El resultado de A/B es: %.2f \n",division);
+            }
+            else
+            {
+                printf("No es posible dividir por cero. \n");
+            }
+            printf("El resultado de A * B es: %.2f \n",multiplicacion);
+            printf("El factorial de %.2f es: %.2f y el factorial de %.2f es: %.2f\n",primerOperando,factorialPrimerNumero,segundoOperando,factorialSegundoNumero);
+            system("pause");
         }
-    }while(opcionMenu!=5);
+        else if(opcionMenu==4 && flagCalculosRealizados==0)
+        {
+            printf("Error. Primero debe realizar los calculos. \n");
+            system("pause");
+        }
+    }
+    while(opcionMenu!=5);
 
 
-if(opcionMenu==5)
-{
-    printf("Calculadora apagada \n");
-}
+    if(opcionMenu==5) //si pongo 1er operando, 2 do y luego salgo ok printf. Si pongo mal las opciones primero y quiero salir no me toma el pritf.
+    {
+        printf("Calculadora apagada \n");
+    }
 
-return 0;
+    return 0;
 }
 
 
@@ -117,8 +150,66 @@ float getFloat(float *resultado,
 }
 
 
-float sumarFloat(float primerNumero, float segundoNumero,float *suma)
+int sumarFloat(float primerNumero, float segundoNumero,float *suma)
 {
-    //int retorno=EXIT_SUCCESS
-    *suma=primerNumero+segundoNumero;
+    int retorno=EXIT_SUCCESS;
+    *suma=primerNumero+segundoNumero;//no entienod el warning.
+    return retorno;
+}
+
+int restarFloat(float primerNumero, float segundoNumero,float *resta)
+{
+    int retorno=EXIT_SUCCESS;
+    *resta=primerNumero-segundoNumero;//no entienod el warning.
+    return retorno;
+}
+
+int dividirFloat(float primerNumero, float segundoNumero,float *division)
+{
+    int retorno = EXIT_ERROR;
+
+    if(segundoNumero!=0)
+    {
+        retorno=EXIT_SUCCESS;
+        *division=primerNumero/segundoNumero;
+
+    }
+    return retorno;
+
+
+}
+
+int multiplicarFloat(float primerNumero, float segundoNumero,float *multiplicacion)
+{
+    int retorno = EXIT_SUCCESS;
+    *multiplicacion=primerNumero*segundoNumero;
+    return retorno;
+
+}
+
+int factorialDeFloat(float primerNumero, float segundoNumero,float *factorialPrimerNumero, float *factorialSegundoNumero)
+{
+    int retorno= EXIT_SUCCESS;
+    int i;
+    int j;
+    if (primerNumero<0)
+    {
+        primerNumero=primerNumero*(-1);
+
+    }
+    if(segundoNumero<0)
+    {
+        segundoNumero=segundoNumero*(-1);
+    }
+    *factorialPrimerNumero=1;
+    *factorialSegundoNumero=1;
+    for(i=1; i<=primerNumero; i++)
+    {
+        *factorialPrimerNumero=*factorialPrimerNumero*i;
+    }
+    for(j=1; j<=segundoNumero; j++)
+    {
+        *factorialSegundoNumero=*factorialSegundoNumero*j;
+    }
+    return retorno;
 }
